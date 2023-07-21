2685ac433e532e7d2f394399178b17859bee9f09
hcyhh <hecongyuan163@163.com>
2023-07-21 22:47:20 +0800

    2023 7 21

--- R包学习长期更新.R
+++ R包学习长期更新.R
@@ -145,16 +145,120 @@ summary(poisson.model.rate)
 #后续参考<https://blog.csdn.net/dege857/article/details/119593047>
 
 
-#
+##SES的LCA分类分析####
+#使用3%样例数据测试
+library('readr')
+library('readxl')
+library('tidyverse')
+data <- read_excel("/Users/hecongyuan/Documents/Temporary/西南队列/样例数据及变量对照表/3%基础样例数据20200611.xlsx")
+#变量生成
+data <- data %>% 
+  mutate(#基础变量生成
+    Occupation=case_when(A7=="1"~"Agricultural_forestry_and_fishery_worker",
+                         A7=="2"~"Retiree",
+                         A7=="3"~"Factory_worker",
+                         A7=="4"~"Homemaker",
+                         A7=="5"~"Administrator",
+                         A7=="6"~"Private_business_owner",
+                         A7=="7"~"Professionals",
+                         A7=="8"~"Laid_off_workers",
+                         A7=="9"~"Sales_and_service_staff",
+                         A7=="10"~"Un-classified"),
+    Marital=case_when(A5==1 ~ 'Married/cohabited',
+                      A5%in%c(2,3,4)~'others',
+                      TRUE ~ 'unknown'),
+    Education=case_when(A6%in%c(1,2) ~ '<6',
+                        A6==3 ~ '6~9',
+                        A6==4 ~ '9~12',
+                        A6%in%c(5,6) ~ '>12',
+                        TRUE ~ 'unknown') ,
+    Household_Income=case_when(A13==1 ~ '<12000',
+                               A13==2 ~ '12000-19999',
+                               A13==3 ~ '20000-59999',
+                               A13==4 ~ '60000-99999',
+                               A13==5 ~ '100000-199999',
+                               A13==6 ~ '>200000',
+                               TRUE ~ 'unknow'),
+    poor_family=case_when(A15==1 ~ '是',
+                         A15==2 ~ '否',
+                         TRUE ~ 'unknow')
 
+  ) %>% 
+  dplyr::select(Occupation,Marital,Education,Household_Income,poor_family) 
 
+#unknow处理NA
+data[data == 'unknow'] <- NA
 
+#查看缺失数据函数
+QS <- function(data){
+  columns <- c(colnames(data))
+  show <- data.frame(matrix(nrow = 0, ncol = length(columns)))
+  colnames(data) = columns
+  colnames(show) <- names(data)
+  for (i in 1:ncol(data)) {
+    show[1,i] <-sum(is.na(data[colnames(data)[i]]))
+  }
+  show
+}
+QS(data)
 
+#删除NA
+data <- na.omit(data)
 
+#初步的统计描述
+stat_desc_2lists<- function(dat) {
+  # Initialize empty lists to store results
+  num_results <- list()
+  char_results <- list()
+  
+  # Get the names of variables in the dataset
+  var_names <- names(dat)
+  
+  # Loop over each variable
+  for (var in var_names) {
+    # Get the type of the variable
+    var_type <- class(dat[[var]])
+    
+    # Perform statistical description based on variable type
+    if (var_type == "numeric") {
+      num_results[[var]] <- summary(dat[[var]])
+    } else if (var_type %in% c("factor", "character")) {
+      if (length(table(dat[[var]])) > 10) {
+        char_results[[var]] <- "Abnormal Variable"
+      } else {
+        char_results[[var]] <- table(dat[[var]])
+      }
+    }
+  }
+  
+  # Return the results lists
+  return(list(Numeric = num_results, Character = char_results))
+}
+results <- stat_desc_2lists(data)
+results$Character
+results$Numeric
 
+ls.str(data)
+#统一修改变变量类型
+cov_factor <- colnames(data)
+data[,cov_factor] <- data.frame(lapply(data[,cov_factor], function(x) as.factor(x)))
 
+#查看所有变量的因子水平
+columns <- colnames(data)
+# 循环遍历所有列
+for (col in columns) {
+  # 判断当前列是否为因子变量
+  if (is.factor(data[[col]])) {
+    # 获取因子变量的分类水平
+    factor_levels <- levels(data[[col]])
+    # 打印输出
+    cat(paste("因子变量", col, "的分类水平:"))
+    print(factor_levels)
+  }
+}
 
-
-
+#更改因子水平
+data$Education <- factor(data$Education, levels = c("<6","6~9","9~12",">12"))
+data$Household_Income <- factor(data$Education, levels = c("<12000","12000-19999","20000-59999","60000-99999","100000-199999",">200000"))
 
 
